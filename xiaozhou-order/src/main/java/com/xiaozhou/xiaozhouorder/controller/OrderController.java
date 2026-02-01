2baa187481f849b3a8342814a692d8f9031f9634
LukeLiner <867976148@qq.com>
2026-02-01 17:05:40 +0800

    增加自定义日志并完成采集

--- xiaozhou-order/src/main/java/com/xiaozhou/xiaozhouorder/controller/OrderController.java
+++ xiaozhou-order/src/main/java/com/xiaozhou/xiaozhouorder/controller/OrderController.java
@@ -1,16 +1,27 @@
 package com.xiaozhou.xiaozhouorder.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @RequestMapping("/getOrder")
-    private String getOrderById(String id) throws InterruptedException {
+    public String getOrderById(@RequestParam(required = false) String id) throws InterruptedException {
+        logger.info("Received getOrder request, orderId={}", id);
+        
+        long startTime = System.currentTimeMillis();
         Thread.sleep(1000);
-        return "orderId:"+id;
+        long duration = System.currentTimeMillis() - startTime;
+        
+        logger.info("Order query completed, orderId={}, duration={}ms", id, duration);
+        
+        return "orderId:" + id;
     }
 }
