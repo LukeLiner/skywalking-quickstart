b0a211dab80c7f1b897b50e9a9e383a4161ed22e
Lukelin <867976148@qq.com>
2026-03-28 11:01:34 +0800

    删除测试脚本

--- check_kibana.ps1
+++
@@ -1,8 +0,0 @@
-$body = @{
-    query = @{
-        match_all = @{}
-    }
-} | ConvertTo-Json
-
-$response = Invoke-RestMethod -Uri 'http://localhost:9200/.kibana/_search?pretty' -Method Post -ContentType 'application/json' -Body $body
-$response | ConvertTo-Json -Depth 5
